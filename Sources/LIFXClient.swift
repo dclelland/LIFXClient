993564be5ee1b184b2e0c1d6c9d9d350763aaf37
Daniel Clelland <daniel.clelland@gmail.com>
2019-02-20 23:28:39 +1300

    Set up LIFXConnection class

--- Sources/LIFXClient.swift
+++ Sources/LIFXClient.swift
@@ -2,46 +2,55 @@
 //  LIFXClient.swift
 //  LIFXClient
 //
-//  Created by Daniel Clelland on 18/02/19.
+//  Created by Daniel Clelland on 20/02/19.
 //
 
 import Foundation
 import Network
 import PromiseKit
 
-public struct LIFXClient {
+public class LIFXClient: LIFXConnection {
     
-    public let source: UInt32
-    public let connection: NWConnection
+    var device: LIFXDevice {
+        return LIFXDevice(connection: connection, source: source)
+    }
     
-    public static func connect(host: NWEndpoint.Host = .ipv4(.broadcast), queue: DispatchQueue = DispatchQueue(label: "LIFXClient Queue"), source: UInt32 = 0) -> Promise<LIFXClient> {
-        return NWConnection(host: host, port: 56700, using: .udp).connect(queue: queue).map { connection in
-            return LIFXClient(source: source, connection: connection)
-        }
+    var light: LIFXLight {
+        return LIFXLight(connection: connection, source: source)
     }
     
 }
 
-extension LIFXClient {
+public class LIFXDevice: LIFXConnection {
     
-    public func send<Request: LIFXEncodableMessage>(_ packet: LIFXPacket<Request>) -> Promise<Void> {
-        return firstly {
-            return connection.send(try LIFXEncoder.encode(packet))
-        }
-    }
+//    - getService
+//    - getHostInfo
+//    - getHostFirmware
+//    - getWifiInfo
+//    - getWifiFirmware
+//    - getPower
+//    - setPower
+//    - getLabel
+//    - setLabel
+//    - getVersion
+//    - getInfo
+//    - getLocation
+//    - setLocation
+//    - getGroup
+//    - setGroup
+//    - echo
     
-    public func receive<Response: LIFXDecodableMessage>(_ type: Response.Type) -> Promise<LIFXPacket<Response>> {
-        return connection.receive().map { data in
-            return try LIFXDecoder.decode(LIFXPacket<Response>.self, data: data)
-        }
-    }
+}
+
+public class LIFXLight: LIFXConnection {
     
-    public func request<Request: LIFXEncodableMessage, Response: LIFXDecodableMessage>(_ packet: LIFXPacket<Request>) -> Promise<LIFXPacket<Response>> {
-        return firstly {
-            return connection.request(try LIFXEncoder.encode(packet))
-        }.map { data in
-            return try LIFXDecoder.decode(LIFXPacket<Response>.self, data: data)
-        }
-    }
+//    - get
+//    - getColor
+//    - getWaveform
+//    - getWaveformOptional
+//    - getPower
+//    - getPower
+//    - getInfrared
+//    - getInfrared
     
 }
---
+++ Sources/LIFXConnection.swift
@@ -0,0 +1,57 @@
+//
+//  LIFXClient.swift
+//  LIFXClient
+//
+//  Created by Daniel Clelland on 18/02/19.
+//
+
+import Foundation
+import Network
+import PromiseKit
+
+public class LIFXConnection {
+    
+    public var connection: NWConnection
+    
+    public var source: UInt32
+    
+    public required init(connection: NWConnection, source: UInt32) {
+        self.connection = connection
+        self.source = source
+    }
+    
+}
+
+extension LIFXConnection {
+    
+    public class func connect<Connection: LIFXConnection>(host: NWEndpoint.Host = .ipv4(.broadcast), queue: DispatchQueue = DispatchQueue(label: "LIFX Queue"), source: UInt32 = 0) -> Promise<Connection> {
+        return NWConnection(host: host, port: 56700, using: .udp).connect(queue: queue).map { connection in
+            return Connection(connection: connection, source: source)
+        }
+    }
+    
+}
+
+extension LIFXConnection {
+    
+    public func send<Request: LIFXEncodableMessage>(_ packet: LIFXPacket<Request>) -> Promise<Void> {
+        return firstly {
+            return connection.send(try LIFXEncoder.encode(packet))
+        }
+    }
+    
+    public func receive<Response: LIFXDecodableMessage>(_ type: Response.Type) -> Promise<LIFXPacket<Response>> {
+        return connection.receive().map { data in
+            return try LIFXDecoder.decode(LIFXPacket<Response>.self, data: data)
+        }
+    }
+    
+    public func request<Request: LIFXEncodableMessage, Response: LIFXDecodableMessage>(_ packet: LIFXPacket<Request>) -> Promise<LIFXPacket<Response>> {
+        return firstly {
+            return connection.request(try LIFXEncoder.encode(packet))
+        }.map { data in
+            return try LIFXDecoder.decode(LIFXPacket<Response>.self, data: data)
+        }
+    }
+    
+}
