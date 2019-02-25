83b3cc2dd89d3897a5639780933c6edf7b8ed909
Daniel Clelland <daniel.clelland@gmail.com>
2019-02-26 02:14:05 +1300

    Set up better light/device API

--- Sources/LIFXClient.swift
+++ Sources/LIFXClient.swift
@@ -12,8 +12,10 @@ import PromiseKit
 public class LIFXClient: LIFXConnection {
     
     public class func connect(host: NWEndpoint.Host, port: NWEndpoint.Port = 56700, queue: DispatchQueue = DispatchQueue(label: "LIFX Queue"), source: UInt32 = UInt32.random()) -> Promise<LIFXClient> {
-        return NWConnection(host: host, port: port, using: .udp).connect(queue: queue).map { connection in
-            return LIFXClient(connection: connection, source: source, target: 0)
+        return NWConnection(host: host, port: port, using: .udp).connect(queue: queue).then { connection in
+            return LIFXClient(connection: connection, source: source, target: 0).requestPacket(LIFXPacket(tagged: true, source: source, target: 0, response: true, message: LIFXDevice.GetService())).map { packet in
+                return LIFXClient(connection: connection, source: source, target: packet.target)
+            }
         }
     }
     
@@ -21,16 +23,12 @@ public class LIFXClient: LIFXConnection {
 
 extension LIFXClient {
     
-    public func getDevice() -> Promise<LIFXDevice> {
-        return requestPacket(LIFXPacket(tagged: true, source: source, target: target, response: true, message: LIFXDevice.GetService())).map { packet in
-            return LIFXDevice(connection: self.connection, source: self.source, target: packet.target)
-        }
+    public var device: LIFXDevice {
+        return LIFXDevice(connection: connection, source: source, target: target)
     }
     
-    public func getLight() -> Promise<LIFXLight> {
-        return requestPacket(LIFXPacket(tagged: true, source: source, target: target, response: true, message: LIFXDevice.GetService())).map { packet in
-            return LIFXLight(connection: self.connection, source: self.source, target: packet.target)
-        }
+    public var light: LIFXLight {
+        return LIFXLight(connection: connection, source: source, target: target)
     }
     
 }
