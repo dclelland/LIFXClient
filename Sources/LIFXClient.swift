696e7c5dcfcbaea4b0ab779a2b39d93ed8a4e7e5
Daniel Clelland <daniel.clelland@gmail.com>
2019-02-26 01:54:36 +1300

    Remove broadcast from LIFXClient

--- Sources/LIFXClient.swift
+++ Sources/LIFXClient.swift
@@ -11,7 +11,7 @@ import PromiseKit
 
 public class LIFXClient: LIFXConnection {
     
-    public class func connect(host: NWEndpoint.Host = .ipv4(.broadcast), port: NWEndpoint.Port = 56700, queue: DispatchQueue = DispatchQueue(label: "LIFX Queue"), source: UInt32 = UInt32.random()) -> Promise<LIFXClient> {
+    public class func connect(host: NWEndpoint.Host, port: NWEndpoint.Port = 56700, queue: DispatchQueue = DispatchQueue(label: "LIFX Queue"), source: UInt32 = UInt32.random()) -> Promise<LIFXClient> {
         return NWConnection(host: host, port: port, using: .udp).connect(queue: queue).map { connection in
             return LIFXClient(connection: connection, source: source, target: 0)
         }
